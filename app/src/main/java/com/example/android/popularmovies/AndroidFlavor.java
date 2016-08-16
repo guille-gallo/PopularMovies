0ad0c610cd430bb528d422080068bf4464b23522
Guillermo Gallo <guillermo.gallo@globant.com>
2016-08-16 12:52:39 -0300

    custom array adapter files

---
+++ app/src/main/java/com/example/android/popularmovies/AndroidFlavor.java
@@ -0,0 +1,15 @@
+package com.example.android.popularmovies;
+
+public class AndroidFlavor {
+    String versionName;
+    String versionNumber;
+    int image; // drawable reference id
+
+    public AndroidFlavor(String vName, String vNumber, int image)
+    {
+        this.versionName = vName;
+        this.versionNumber = vNumber;
+        this.image = image;
+    }
+
+}
